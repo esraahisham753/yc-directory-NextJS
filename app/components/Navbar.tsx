7c83e382572c37f45ca089031aeac2da35a5593b
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-25 12:28:00 +0300

    Update header style and add tailwindcss config

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = async () => {
   const session = await auth()
 
   return (
-    <header className="bg-white p-4 font-worksans">
+    <header className="bg-[#F7F7F7] p-4 font-worksans">
         <nav className="flex justify-between items-center">
             <Link href="/">
                 <Image src="/logo.png" alt="logo" width={143} height={30}/>
@@ -22,7 +22,7 @@ const Navbar = async () => {
                             }}>
                                 <button type="submit" className="text-[#EF4444] font-semibold text-xl cursor-pointer">Logout</button>
                             </form>
-                            <Link href={`user/${session?.id}`}>
+                            <Link href={`user/${session?.user?.id}`}>
                                 <span>{session?.user?.name}</span>
                             </Link>
                         </>
