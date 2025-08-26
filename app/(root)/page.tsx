e13bdedc85a4af1038384c75d45ca3fea700863b
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-26 18:16:35 +0300

    Fix globals.css and add Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="heading">Home</h1>
+    <>
+      <section className="pink-container">
+        <h1 className="heading">Pitch Your Startup, <br />Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      </section>
+    </>
   );
 }
